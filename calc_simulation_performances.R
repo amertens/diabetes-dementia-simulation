6c73621b00be9842bd7d57a42aab53d245fcf912
Andrew Mertens <amertens@berkeley.edu>
2023-05-19 14:01:11 -0700

    update

--- calc_simulation_performances.R
+++ calc_simulation_performances.R
@@ -3,9 +3,9 @@
 
 rm(list=ls())
 source(here::here("0_config.R"))
-source(paste0(here::here(),"/0_ltmle_Estimate_update.R"))
-source(paste0(here::here(),"/simulation study/0_simulation_functions.R"))
-source(paste0(here::here(),"/simulation study/0_simulation_cleaning_functions.R"))
+source(paste0(here::here(),"/functions/0_ltmle_Estimate_update.R"))
+source(paste0(here::here(),"/functions/0_simulation_functions.R"))
+source(paste0(here::here(),"/functions/0_simulation_cleaning_functions.R"))
 
 
 
@@ -26,101 +26,42 @@ files <- dir(path=paste0(here::here(),"/sim_res/null/"), pattern = "*.RDS")
 
 
 #load bootstrap
-boot_iter_files <- dir(path=paste0(here::here(),"/data/bootstrap/"), pattern = "*.RDS")
-boot_iter_files <- boot_iter_files[grepl("sim_res_boot_old_sim_null_T11_",boot_iter_files)]
+boot_iter_files <- dir(path=paste0(here::here(),"/sim_res/null//bootstrap/"), pattern = "*.RDS")
 length(boot_iter_files)
 
 
 trueRR=1
 trueRD=0
 iptw=T
-original_sim_res_null <- calc_sim_performance(files=files, boot_iter_files=boot_iter_files, trueRR=1, trueRD=0, iptw=T)
-original_sim_res_null$perf_tab_RR
-original_sim_res_null$perf_tab_diff
-original_sim_res_null$perf_tab_diff$filenames
+sim_res_null <- calc_sim_performance(files=files, filepath="/sim_res/null/", boot_iter_files=boot_iter_files, trueRD=0, iptw=T)
+sim_res_null$filenames
 
 
-res_null_diff <- original_sim_res_null$perf_tab_diff
-res_null_RR <- original_sim_res_null$perf_tab_RR
 
-save(res_null_diff, res_null_RR,  file=paste0(here::here(),"/results/sim_performance_results_original_null.Rdata"))
+saveRDS(sim_res_null,  file=paste0(here::here(),"/results/sim_performance_results_null.RDS"))
 
 
-#explore answer to Maya's comments:
-df <- res_null_diff %>% filter(iptw=="IPTW", estimator=="GLM")
-df
-
-
-df <- res_null_diff %>% filter(iptw=="IPTW", estimator=="LASSO", variance_estimator=="ic")
-df
-
-df <- res_null_diff %>% filter(o.coverage > 95)
-df
 
 
 #---------------------------------------------------------
 # manuscript sim results: protective sim
 #---------------------------------------------------------
-files <- dir(path=paste0(here::here(),"/sim_res/"), pattern = "*.RDS")
-#files <- files[grepl("_v3",files)]
-files <- files[!grepl("_null_",files)]
-
-#NOTE: check which files are missing from the old pipeline:
-old_files <- dir(path=paste0(here::here(),"/sim_res_old/"), pattern = "*.RDS")
-old_files <- old_files[grepl("_v3",old_files)]
-old_files <- gsub("_v3","",old_files)
+files <- dir(path=paste0(here::here(),"/sim_res/protective/"), pattern = "*.RDS")
 
 
-files_temp <- files[!(files %in% c( "sim_res_1se.RDS","sim_res_AUC.RDS" ))]
-old_files <- old_files[!(old_files %in% c( "sim_res_1se_ic.RDS","sim_res_AUC_ic.RDS" ))]
 
+boot_iter_files <- dir(path=paste0(here::here(),"/sim_res/bootstrap/protective/"), pattern = "*.RDS")
 
-files_temp <- gsub(".RDS","", files_temp)
-old_files <- gsub("_T11","", old_files)
-
-files_temp[!(files_temp %in% old_files)]
-old_files[!(old_files %in% files_temp)]
-
-
-boot_iter_files <- dir(path=paste0(here::here(),"/data/bootstrap/"), pattern = "*.RDS")
-unique(gsub("\\d","",boot_iter_files))
-
-boot_iter_files_500iter <- boot_iter_files[grepl("sim_res_boot_old_sim_cens_competing_risks_500_iter_T11",boot_iter_files)]
-boot_iter_files <- boot_iter_files[grepl("_v3",boot_iter_files)|grepl("sim_res_boot_original_sim_T11_",boot_iter_files)|grepl("sim_res_boot_old_sim_detQ_T11",boot_iter_files)]
-#boot_iter_files <- boot_iter_files[!grepl("ridge",boot_iter_files)]
-boot_iter_files <- c(boot_iter_files, boot_iter_files_500iter)
-boot_iter_files <- data.frame(boot_file = boot_iter_files, analysis=NA)
-boot_iter_files$analysis[grepl("500_iter",boot_iter_files$boot_file)] <- "DetQ, 500 iter"
-boot_iter_files$analysis[grepl("sim_res_boot_old_sim_detQ_T11",boot_iter_files$boot_file)] <- "DetQ"
-boot_iter_files$analysis[grepl("ridge",boot_iter_files$boot_file)] <- "Ridge"
-boot_iter_files$analysis[grepl("cens_competing_risks_v3",boot_iter_files$boot_file) | grepl("sim_res_boot_original_sim_T11_",boot_iter_files$boot_file)] <- "no DetQ"
-table(boot_iter_files$analysis)
+truth <- readRDS(paste0(here::here(),"/data/sim_res_truth.RDS"))
+trueRD <- truth[10,2]
 
 
 
 
 
 
-trueRR=0.5148661
-trueRD= -0.009683665
+sim_res_protective <- calc_sim_performance(files=files, filepath="/sim_res/protective/", boot_iter_files=boot_iter_files,  trueRD= trueRD, iptw=T )
 
+saveRDS(sim_res_protective,  file=paste0(here::here(),"/results/sim_performance_results_protective.RDS"))
 
-files=files
-boot_iter_files=boot_iter_files
-trueRR=trueRR
-trueRD= trueRD
-iptw=T
-
-
-res_v3 <- calc_sim_performance(files=files, boot_iter_files=boot_iter_files, trueRR=trueRR, trueRD= trueRD, iptw=T )
-
-
-res_v3_diff <- res_v3$perf_tab_diff
-res_v3_RR <- res_v3$perf_tab_RR
-
-#remove duplicates /iptw repeats
-res_v3_diff <- res_v3_diff %>% filter(!filenames %in% c("sim_res_ridge_ic_v3_iptw","sim_res_ridge","sim_res_Qint_ic","sim_res_Qint_ic_v3_iptw",
-                                                        "sim_res_DetQ__ridge_ic_v3_iptw","sim_res_ridge_ic_v3","sim_res_DetQ_ic_v3_iptw","sim_res_ic"))
-
-save(res_v3_diff, res_v3_RR,  file=paste0(here::here(),"/results/sim_performance_results_original.Rdata"))
 
--- functions/0_simulation_cleaning_functions.R
+++ functions/0_simulation_cleaning_functions.R
@@ -13,9 +13,9 @@
 # trueRR=0.3930283
 # trueRD=-0.035852
 
-calc_sim_performance <- function(files, boot_iter_files=NULL, trueRR, trueRD, iptw=F){
+calc_sim_performance <- function(files, filepath, boot_iter_files=NULL, trueRD, iptw=F){
 
-  setwd(paste0(here::here(),"/sim_res/"))
+  setwd(paste0(here::here(),filepath))
   d <- files %>% map(readRDS) %>% map_dfr(~bind_rows(.) , .id="analysis")
   d <- d %>% mutate(analysis = factor(analysis))
   levels(d$analysis) = files[as.numeric(levels(d$analysis))]
@@ -36,29 +36,7 @@ calc_sim_performance <- function(files, boot_iter_files=NULL, trueRR, trueRD, ip
     d <- bind_rows(d, d.iptw)
   }
 
-  d$true.RR <- trueRR
   d$true.RD <- trueRD
-
-  # head(d)
-  perf_tab_RR <- d %>% group_by(analysis) %>%
-    mutate(variance=mean(((estimate)-mean((estimate)))^2),
-           o.ci.lb = (estimate) - 1.96 * sqrt(variance),
-           o.ci.ub = (estimate) + 1.96 * sqrt(variance)) %>%
-    group_by(analysis) %>%
-    summarize(
-      bias=mean(log(estimate))-log(true.RR),
-      variance=mean((mean(log(estimate))-log(estimate))^2),
-      mse = bias^2 + variance,
-      bias_se_ratio= bias/sqrt(variance),
-      bias_se_ratio_emp= bias/mean(std.dev),
-      coverage=mean(CI.2.5.<=true.RR & true.RR<=CI.97.5.)*100,
-      #oracle coverage
-      o.coverage=mean(o.ci.lb<=(true.RR) & (true.RR)<= o.ci.ub)*100,
-      mean_ci_width=mean(log(CI.97.5.)-log(CI.2.5.)),
-      power=mean((CI.2.5. > 1 & CI.97.5.>1)|(CI.2.5. < 1 & CI.97.5.<1))*100
-    ) %>% filter(!is.na(variance)) %>%
-    distinct()
-
 
   perf_tab_diff <- d %>% filter(!is.na(ate)) %>%
     subset(., select = -c(estimate, std.dev, CI.2.5., CI.97.5.)) %>%
@@ -84,7 +62,7 @@ calc_sim_performance <- function(files, boot_iter_files=NULL, trueRR, trueRD, ip
     distinct()
 
   if(!is.null(boot_iter_files)){
-    setwd(paste0(here::here(),"/data/bootstrap/"))
+    setwd(paste0(here::here(),filepath,"/bootstrap/"))
 
     if( class(boot_iter_files) == "data.frame"){
       boot_iter_files$boot_iter <- as.character(1:nrow(boot_iter_files))
@@ -117,12 +95,6 @@ calc_sim_performance <- function(files, boot_iter_files=NULL, trueRR, trueRD, ip
           ate.CI2=quantile(ate,.975)
         )
 
-      perf_tab_RR_boot <- boot_CIs %>% group_by(analysis) %>%
-        summarize(
-          coverage = mean(CI1 <= d$true.RR[1] &  CI2 >= d$true.RR[1])*100,
-          mean_ci_width=mean(log(CI2)-log(CI1)),
-          power=mean((CI1 > 1 & CI2>1)|(CI1 < 1 & CI2<1))*100
-        ) %>% as.data.frame()
 
       perf_tab_diff_boot <- boot_CIs %>% group_by(analysis) %>%
         summarize(
@@ -134,8 +106,6 @@ calc_sim_performance <- function(files, boot_iter_files=NULL, trueRR, trueRD, ip
     }else{
       boot_CIs <- boot_res %>% group_by(iteration) %>%
         summarise(
-          CI1=quantile(estimate,.025),
-          CI2=quantile(estimate,.975),
           ate.CI1=quantile(ate,.025),
           ate.CI2=quantile(ate,.975)
         )
@@ -143,13 +113,6 @@ calc_sim_performance <- function(files, boot_iter_files=NULL, trueRR, trueRD, ip
 
 
       #calculate bootstrap performance
-      perf_tab_RR_boot <- data.frame(
-        coverage = mean(boot_CIs$CI1 <= d$true.RR[1] &  boot_CIs$CI2 >= d$true.RR[1])*100,
-        mean_ci_width_logRR=mean(log(boot_CIs$CI2)-log(boot_CIs$CI1)),
-        power=mean((boot_CIs$CI1 > 1 & boot_CIs$CI2>1)|(boot_CIs$CI1 < 1 & boot_CIs$CI2<1))*100
-      )
-      perf_tab_RR_boot
-
       perf_tab_diff_boot <- data.frame(
         coverage = mean(boot_CIs$ate.CI1 <= d$true.RD[1] &  boot_CIs$ate.CI2 >= d$true.RD[1])*100,
         mean_ci_width=mean((boot_CIs$ate.CI2)-(boot_CIs$ate.CI1)),
@@ -157,19 +120,15 @@ calc_sim_performance <- function(files, boot_iter_files=NULL, trueRR, trueRD, ip
       )
     }
 
-    perf_tab_RR <- bind_rows(perf_tab_RR, perf_tab_RR_boot)
     perf_tab_diff <- bind_rows(perf_tab_diff, perf_tab_diff_boot)
   }
 
-  #perf_tab_diff <- perf_tab_diff %>% filter(!grepl("iptw",analysis))
 
-  perf_tab_RR <- clean_sim_results(perf_tab_RR)
   perf_tab_diff <- clean_sim_results(perf_tab_diff)
 
-  perf_tab_RR$iptw <- ifelse(grepl("iptw", perf_tab_RR$filenames), "IPTW", "TMLE")
   perf_tab_diff$iptw <- ifelse(grepl("iptw", perf_tab_diff$filenames), "IPTW", "TMLE")
 
-  return(list(perf_tab_RR=perf_tab_RR, perf_tab_diff=perf_tab_diff))
+  return(perf_tab_diff)
 }
 
 
